e9744b6d3dea44a6bae36d3a5d25878f7853d0b7
liwuv587 <wolferhua@blockpi.io>
2022-08-07 22:56:35 +0800

    feat code style

--- cmd/ffs/commands/ffs.go
+++ cmd/ffs/commands/ffs.go
@@ -7,37 +7,20 @@ import (
 )
 
 var ffsCommand = cli.Command{
-	Name:         "ffs",
-	ShortName:    "",
-	Aliases:      []string{"run"},
-	Usage:        "",
-	UsageText:    "",
-	Description:  "",
-	ArgsUsage:    "",
-	Category:     "",
-	BashComplete: nil,
-	Before: func(cmd *cli.Context) error {
-		logger, _ := zap.NewDevelopment()
-		zap.ReplaceGlobals(logger)
-		return nil
-	},
-	After: nil,
-	Action: func(cmd *cli.Context) error {
-		zap.L().Debug("service start", zap.Int("port", config.Port))
-		return nil
-	},
-	OnUsageError: nil,
-	Subcommands:  nil,
+	Name:    "ffs",
+	Aliases: []string{"run"},
 	Flags: []cli.Flag{
 		config.FlagPort,
 		config.FlagMulticore,
 		config.FlagDebug,
 	},
-	SkipFlagParsing:        false,
-	SkipArgReorder:         false,
-	HideHelp:               false,
-	Hidden:                 false,
-	UseShortOptionHandling: false,
-	HelpName:               "",
-	CustomHelpTemplate:     "",
+	Before: func(cmd *cli.Context) error {
+		logger, _ := zap.NewDevelopment()
+		zap.ReplaceGlobals(logger)
+		return nil
+	},
+	Action: func(cmd *cli.Context) error {
+		zap.L().Debug("service start", zap.Int("port", config.Port))
+		return nil
+	},
 }
